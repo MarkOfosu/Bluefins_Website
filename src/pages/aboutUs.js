36e2b6db132ad26a3face40d01da955fe1e0392a
MarkOfosu <marko.twum@gmail.com>
2023-02-28 16:00:19 -0800

    added informatio to the about us pagen

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,7 +1,51 @@
 import React from "react";
 
 const AboutUsPage = () => {
-   return  <div>About Us</div>
+   return (
+      <div>
+         
+         <h1>Who We Are</h1>
+         <p>Ghana BlueFins offers unique swimming lessons and programs for all ages and abilities.</p>
+         <p> We currently offer 3 different programs in Accra, depending on your desires and swimming level.</p>
+         <ol>
+            <li>Competitve Swim Team</li>
+            <li>Learn-to-Swim for Leisure</li>
+            <li>Learn-to-Swim for Competitive Swimming</li> 
+         </ol>
+
+         <h1>Mission Statement</h1>
+         <p>Our mission is to create and sustain a nationally recognized, competitive swimming program and to create awareness in swimming as a lifesaving skill. 
+            We embody the highest quality and consistency of coaching. 
+            We emphasize family participation and the involvement of all ability levels. 
+            We promote a sense of community, cooperation, and sportsmanship in an inviting, inclusive, and supportive environment, 
+            while encouraging each swimmer to maximize their full potential and have fun.
+         </p>
+         
+         <h1>Vision Statement</h1>
+         <p>
+          Our vision is to promote self-discipline and self-improvement.
+          We are committed to providing excellence, the spirit of sportsmanship, 
+          and a supportive and productive team environment.
+         </p>
+
+         <h1>Code of Ethics</h1>
+         <p>
+            Swimmers will not be mean/disrespectful to each other, use inappropriate words, or challenge coaches during training sessions. Swimmers will not be late to practice - if this is unavoidable, swimmers must walk as brisk as possible to the pool. All swimmers must have drinking water during training sessions.
+
+            Parents will not interrupt or interfere with children or the coaches during training sessions. Parents will not act as the coaches to their children.
+
+            All concerns can be addressed to the head coach privately.
+         </p>
+
+      </div>
+
+        
+
+      
+  
+   
+  
+   )
 }
 
 export default AboutUsPage;
